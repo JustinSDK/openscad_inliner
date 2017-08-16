d1a792060512aa028f95471a24badd438df6f4b3
Justin Lin <caterpillar@openhome.cc>
2017-08-16 09:50:22 +0800

    refactored into a module

--- inline.py
+++ inline.py
@@ -1,59 +1,10 @@
-import sys, re, os
-from typing import List, Set, Pattern
-
-regex = re.compile(r'<(.*)>')             # type: Pattern[str]
-OPENSCADPATH = os.environ['OPENSCADPATH'] # type: str
-library_declation = '''
-    
-/**
- * The dotSCAD library
- * @copyright Justin Lin, 2017
- * @license https://opensource.org/licenses/lgpl-3.0.html
- *
- * @see https://github.com/JustinSDK/dotSCAD
-*/
-
-'''
-
-def included_scads_in(scad: str) -> Set[str]:
-    with open(scad) as src:
-        return {
-            os.path.join(OPENSCADPATH, regex.findall(line)[0])
-                for line in src if line.startswith('include')
-        }
-
-def all_scads_from(main_scad: str) -> Set[str]:
-    def dig_all_scads(undug_scads: Set[str]) -> Set[str]:
-        if not undug_scads:
-            return set()
-        else:
-            sub_scads = {
-                    sub_scad for undug_scad in undug_scads
-                        for sub_scad in included_scads_in(undug_scad)
-            }
-            return undug_scads | dig_all_scads(sub_scads - undug_scads)
-
-    scads_in_main = included_scads_in(main_scad)
-    return {scad for scad in dig_all_scads(scads_in_main)}
-
-def code_in(scad: str) -> List[str]:
-    with open(scad) as f:
-        return [line for line in f if not line.startswith('include')]
-
-def inliner(src_scad: str, dest_scad: str) -> None:
-    with open(dest_scad, mode = 'w') as f:
-        f.writelines(code_in(src_scad))
-        f.write(library_declation)
-
-        for scad in all_scads_from(src_scad):
-            f.writelines(code_in(scad))
-            f.write('\n\n')   
+import sys, scad
 
 def main() -> None:
     try:
         src_scad = sys.argv[1]  
         dest_scad = sys.argv[2] 
-        inliner(src_scad, dest_scad)
+        scad.inliner(src_scad, dest_scad)
     except IndexError:
         print('Usage:\n\tpython inline.py src.scad dest.scad')
 
---
+++ scad.py
@@ -0,0 +1,50 @@
+import re, os
+from typing import List, Set, Pattern
+
+regex = re.compile(r'<(.*)>')             # type: Pattern[str]
+OPENSCADPATH = os.environ['OPENSCADPATH'] # type: str
+library_declation = '''
+    
+/**
+ * The dotSCAD library
+ * @copyright Justin Lin, 2017
+ * @license https://opensource.org/licenses/lgpl-3.0.html
+ *
+ * @see https://github.com/JustinSDK/dotSCAD
+*/
+
+'''
+
+def included_scads_in(scad: str) -> Set[str]:
+    with open(scad) as src:
+        return {
+            os.path.join(OPENSCADPATH, regex.findall(line)[0])
+                for line in src if line.startswith('include')
+        }
+
+def all_scads_from(main_scad: str) -> Set[str]:
+    def dig_all_scads(undug_scads: Set[str]) -> Set[str]:
+        if not undug_scads:
+            return set()
+        else:
+            sub_scads = {
+                    sub_scad for undug_scad in undug_scads
+                        for sub_scad in included_scads_in(undug_scad)
+            }
+            return undug_scads | dig_all_scads(sub_scads - undug_scads)
+
+    scads_in_main = included_scads_in(main_scad)
+    return {scad for scad in dig_all_scads(scads_in_main)}
+
+def code_in(scad: str) -> List[str]:
+    with open(scad) as f:
+        return [line for line in f if not line.startswith('include')]
+
+def inliner(src_scad: str, dest_scad: str) -> None:
+    with open(dest_scad, mode = 'w') as f:
+        f.writelines(code_in(src_scad))
+        f.write(library_declation)
+
+        for scad in all_scads_from(src_scad):
+            f.writelines(code_in(scad))
+            f.write('\n\n')   
