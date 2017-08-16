162b30dcdb43580d3c023f450558043f75edcc16
Justin Lin <caterpillar@openhome.cc>
2017-08-16 11:23:38 +0800

    added -op argument

--- inline.py
+++ inline.py
@@ -1,14 +1,30 @@
-import sys, argparse 
+import sys, os, argparse
 import scad
+from typing import List
+
+def do_inline(src, dest, scad_paths):
+    try:
+        scad.inliner(src, dest, scad_paths)
+    except FileNotFoundError as e:
+        print(e)
+        print('Try to use the -op argument or set up the OPENSCADPATH environment variable')
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("src", help="the source scad")
-    parser.add_argument("dest", help="the destination scad")
+    parser.add_argument("src", help="The source")
+    parser.add_argument("dest", help="The destination")
+    parser.add_argument("-op", "--openscadpath", help="Specify where to find user scad files")
 
     if(len(sys.argv) == 1):
         parser.print_help()
     else:
-        scad.inliner(args.src, args.dest)
+        args = parser.parse_args()
+
+        scad_paths: List[str] = (
+            args.openscadpath 
+                if args.openscadpath else os.environ['OPENSCADPATH']
+        ).split(';')
+
+        do_inline(args.src, args.dest, scad_paths)
 
 main()
--- scad.py
+++ scad.py
@@ -2,7 +2,6 @@ import re, os
 from typing import List, Set, Pattern
 
 regex = re.compile(r'<(.*)>')             # type: Pattern[str]
-OPENSCADPATH = os.environ['OPENSCADPATH'] # type: str
 library_declation = '''
     
 /**
@@ -15,36 +14,51 @@ library_declation = '''
 
 '''
 
-def included_scads_in(scad: str) -> Set[str]:
+def scad_file_fullname(scad_paths: List[str], scad_name: str) -> str:
+    for path in scad_paths:
+        f = os.path.join(path, scad_name)
+        if os.path.isfile(f):
+            return f
+
+    raise FileNotFoundError('scad not found: {}'.format(scad_name))
+
+def included_scads_in(scad: str, scad_paths: List[str] = None) -> Set[str]:
+    scads = None
     with open(scad) as src:
-        return {
-            os.path.join(OPENSCADPATH, regex.findall(line)[0])
+        scads = {
+            regex.findall(line)[0]
                 for line in src if line.startswith('include')
         }
 
-def all_scads_from(main_scad: str) -> Set[str]:
+    if scad_paths:
+        return {scad_file_fullname(scad_paths, scad) for scad in scads}
+    else: # find scads in the current folder
+        return scads
+
+def all_scads_from(main_scad: str, scad_paths: List[str] = None) -> Set[str]:
     def dig_all_scads(undug_scads: Set[str]) -> Set[str]:
         if not undug_scads:
             return set()
         else:
             sub_scads = {
                     sub_scad for undug_scad in undug_scads
-                        for sub_scad in included_scads_in(undug_scad)
+                        for sub_scad in included_scads_in(undug_scad, scad_paths)
             }
             return undug_scads | dig_all_scads(sub_scads - undug_scads)
 
-    scads_in_main = included_scads_in(main_scad)
+    scads_in_main = included_scads_in(main_scad, scad_paths)
+    
     return {scad for scad in dig_all_scads(scads_in_main)}
 
 def code_in(scad: str) -> List[str]:
     with open(scad) as f:
         return [line for line in f if not line.startswith('include')]
 
-def inliner(src_scad: str, dest_scad: str) -> None:
+def inliner(src_scad: str, dest_scad: str, scad_paths: List[str] = None) -> None:
     with open(dest_scad, mode = 'w') as f:
         f.writelines(code_in(src_scad))
         f.write(library_declation)
 
-        for scad in all_scads_from(src_scad):
+        for scad in all_scads_from(src_scad, scad_paths):
             f.writelines(code_in(scad))
             f.write('\n\n')   
